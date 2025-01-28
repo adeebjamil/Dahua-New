0bfead0a903e53fcdcbfecd4ca7f1e5a704465e0
adeeb jamil <2100032535@kluniversity.in>
2025-01-28 13:40:58 +0530

    major update

---
+++ src/lib/products.js
@@ -0,0 +1,210 @@
+
+const products = [
+
+  // Panoramic Series -> Hubble Series -> 6 Products
+
+  {
+    id: 'PSDW83242M-A360-D845L-S3',
+    name: '32 MP Multi-Sensor 360° Panoramic PTZ Hubble WizMind Network Camera',
+    image: '/src/assets/WizMind/img1.png',
+    description: `
+     Panoramic parameters: 8 × 4 MP 1/1.8" CMOS image sensors Outputs max. 32 MP (11520 × 2700)@25/30 fps.
+     PTZ parameters: 1 × 8 MP 1/1.8" CMOS image sensors. Outputs max. 8 MP (3840 × 2160)@25/30 fps.
+     Channel 2 (PTZ): Built-in illuminators (2 × IR and 7 × laser), max. illumination distance: 500 m.
+     Channel 2 (PTZ): 45x optical zoom,16x digital zoom.
+     H.265 codec, high compression rate, ultra-low bit rate.
+     3D NR, HLC, BLC, applicable to various monitoring scenes.
+     ROI, SMART H.264+/H.265+, flexible coding, applicable to various bandwidth and storage environments.
+     Alarm: 7 in, 3 out; audio: 2 in, 2 out; RS-485; BNC; supports max. 512 G SD card.
+     Channel 1 (Panoramic): Three switchable intelligent resources: Perimeter protection, crowd map, vehicle density.
+     Channel 2 (PTZ): Three switchable intelligent resources: Perimeter protection, video metadata, face recognition.
+     GPS/BDS positioning.
+  `,
+    category: 'Panoramic Series',
+    subCategory: 'Hubble Series',
+    brochure: '/brochures/brousher.pdf',
+    additionalImage: '/src/assets/descimg/img1.png'
+  },
+  {
+    id: 'PSDW8842S-A180-D425',
+    name: '8 MP Multi-Sensor 180° Panoramic PTZ Mini Hubble Wizmind Network Camera',
+    image: '/src/assets/WizMind/img1.png',
+    description: `
+    • Panoramic parameters: 4 × 2 MP 1/1.8" CMOS image sensors.
+    • PTZ parameters: 1 × 4 MP 1/1.8" CMOS image sensors.
+    • PTZ camera: 25× optical zoom,16× digital zoom.
+    • H.265 codec, high compression rate, ultra-low bit rate.
+    • WDR, 3D NR, HLC, BLC, applicable to various monitoring scenes.
+    • ROI, SMART H.264+/H.265+, flexible coding, applicable to various bandwidth and storage environments.
+    • Smart tracking.
+    • Built-in dual MIC, built-in speaker, support two-way talk.
+    • Intelligent Functions: Panoramic Network Camera: Perimeter Protection; PTZ Camera: Perimeter Protection, Face Detection and Video Metadata.
+`,
+    category: 'Panoramic Series',
+    subCategory: 'Hubble Series',
+    brochure: '/brochures/brousher.pdf',
+    additionalImage: '/src/assets/descimg/img2.png'
+  },
+  {
+    id: 'PSDW81642M-A360-D440-S3',
+    name: '16 MP Multi-Sensor 360° Panoramic PTZ Hubble WizMind Network Camera',
+    image: '/src/assets/WizMind/img1.png',
+    description: `
+    • Channel 1 (Panoramic): 8 × 2 MP 1/1.8" CMOS image sensors. Field of View:Horizontal: 1 × 360°, Vertical: 103°. Channel 2 (PTZ): 1 × 4 MP 1/1.8" CMOS image sensors.
+    • Channel 2 (PTZ): 40x optical zoom, 16x digital zoom.
+    • Channel 2 (PTZ): Illumination distance up to 400 m.
+    • Channel 1 (Panoramic): Three switchable intelligent resources: Perimeter protection, Crowd map, Vehicle density.
+    • Channel 2 (PTZ): Three switchable intelligent resources: Perimeter protection, Video metadata, Face recognition.
+    • AR panorama technology; various AR tags can be overlaid.
+    • Smart tracking. GPS/BDS positioning.
+    • Smart H265+/H264+ encoding.
+    • WDR, 3D NR, HLC, BLC, applicable to various monitoring scenes.
+    • Alarm: 7 in, 3 out; audio: 2 in, 2 out; 1 channel BNC, 1 channel RS–485 (baud rate can be set). supports max. 512 G SD card.
+`,
+    category: 'Panoramic Series',
+    subCategory: 'Hubble Series',
+    brochure: '/brochures/brousher.pdf',
+    additionalImage: '/src/assets/descimg/img3.png'
+  },
+  {
+    id: 'PSDW81642M-A180-D440-S3',
+    name: '16 MP Multi-Sensor 180° Panoramic PTZ Hubble WizMind Network Camera',
+    image: '/src/assets/WizMind/img1.png',
+    description: `
+    • Channel 1 (Panoramic): 4 × 4 MP 1/1.8" CMOS image sensors. Field of View:Horizontal: 1 × 180°: Vertical: 103°. Channel 2 (PTZ): 1 × 4 MP 1/1.8" CMOS image sensors.
+    • Channel 2 (PTZ): 40x optical zoom, 16x digital zoom.
+    • Channel 2 (PTZ): Illumination distance up to 400 m.
+    • Channel 1 (Panoramic): Three switchable intelligent resources: Perimeter protection, crowd map, vehicle density.
+    • Channel 2 (PTZ): Three switchable intelligent resources: Perimeter protection, video metadata, face recognition.
+    • AR panorama technology; various AR tags can be overlaid.
+    • Smart tracking. GPS/BDS positioning.
+    • Smart H265+/H264+ encoding.
+    • WDR, 3D NR, HLC, BLC, applicable to various monitoring scenes.
+    • Alarm: 7 in, 3 out; audio: 2 in, 2 out; 1 channel BNC, 1 channel RS–485 (baud rate can be set). supports max. 512 G SD card.
+`,
+    category: 'Panoramic Series',
+    subCategory: 'Hubble Series',
+    brochure: '/brochures/brousher.pdf',
+    additionalImage: '/src/assets/descimg/img3.png'
+  },
+  {
+    id: 'PSDW82442M-A270-D440-S3',
+    name: '24 MP Multi-Sensor 270° Panoramic PTZ Hubble WizMind Network Camera',
+    image: '/src/assets/WizMind/img1.png',
+    description: `
+    • Channel 1 (Panoramic): 6 × 4 MP 1/1.8" CMOS image sensors. Field of View:Horizontal: 1 × 270°, Vertical: 103°. Channel 2 (PTZ): 1 × 4 MP 1/1.8" CMOS image sensors.
+    • Channel 2 (PTZ): 40x optical zoom, 16x digital zoom.
+    • Channel 2 (PTZ): Illumination distance up to 400 m.
+    • Channel 1 (Panoramic): Three switchable intelligent resources: Perimeter protection, Crowd map, Vehicle density.
+    • Channel 2 (PTZ): Three switchable intelligent resources: Perimeter protection, Video metadata, Face recognition.
+    • AR panorama technology; various AR tags can be overlaid.
+    • Smart tracking. GPS/BDS positioning.
+    • Smart H265+/H264+ encoding.
+    • WDR, 3D NR, HLC, BLC, applicable to various monitoring scenes.
+    • Alarm: 7 in, 3 out; audio: 2 in, 2 out; 1 channel BNC, 1 channel RS–485 (baud rate can be set). supports max. 512 G SD card.
+`,
+    category: 'Panoramic Series',
+    subCategory: 'Hubble Series',
+    brochure: '/brochures/brousher.pdf',
+    additionalImage: '/src/assets/descimg/img4.png'
+  },
+  {
+    id: 'PSDW81642S-A360-D425',
+    name: '16 MP Multi-Sensor 360° Panoramic PTZ Mini Hubble Wizmind Network Camera',
+    image: '/src/assets/WizMind/img1.png',
+
+
+    description: `
+    • Panoramic parameters: 8 × 2 MP 1/1.8" CMOS image sensors. PTZ parameters: 1 × 4 MP 1/1.8" CMOS image sensors.
+    • PTZ camera: Built-in laser illuminator, max. illumination distance: 100 m.
+    • PTZ camera: 25x optical zoom,16x digital zoom.
+    • H.265 codec, high compression rate, ultra-low bit rate.
+    • 3D NR, HLC, BLC, applicable to various monitoring scenes.
+    • ROI, SMART H.264+/H.265+, flexible coding, applicable to various bandwidth and storage environments.
+    • Smart tracking.
+    • Built-in dual MIC and dual speaker, support two-way talk.
+    • Intelligent Functions: Panoramic Network Camera: Perimeter Protection; PTZ Camera: Perimeter Protection, Face Detection and Video Metadata.
+`,
+    category: 'Panoramic Series',
+    subCategory: 'Hubble Series',
+    brochure: '/brochures/brousher.pdf',
+    additionalImage: '/src/assets/descimg/img4.png'
+  },
+
+
+
+// Panoramic Series -> Multi-Sensor 180° Series -> 2 Products 
+
+
+  {
+    id: 'IPC-PFW83242-A180-S2',
+    name: '4 × 8 MP Multi-Sensor Panoramic Bullet WizMind Network Camera',
+    image: '/src/assets/WizMind/img2.png',
+
+
+    description: `
+    • 4 × 8MP, 1/1.8" CMOS image sensor, low illuminance, high image definition
+    • Outputs max. 32MP (8192 × 3840) @25/30 fps; with intelligent functions enabled, it can output 32MP (8192 × 3840) @20 fps
+    • Built-in IR LED, and the max. illumination distance is 30 m.
+    • ROI, SMART H.264+/H.265+, flexible coding, applicable to various bandwidth and storage environments.
+    • Intelligent functions: Crowd Distribution Map, Vehicle Density, Perimeter Protection, EPTZ.
+    • Support image correction.
+    • Alarm: 2 in, 2 out; audio: 1 in, 1 out; 485; BNC; supports max. 256G SD card.
+    • 12 VDC/24 VAC/PoE power supply; easy for installation
+    • IP67 and IK10 protection
+    • AR panorama technology; various AR tags can be overlaid.
+`,
+
+    category: 'Panoramic Series',
+    subCategory: 'Multi-Sensor 180° Series',
+    brochure: '/brochures/brousher.pdf',
+    additionalImage: '/src/assets/descimg/img5.png'
+  },
+
+  {
+    id: 'IPC-PFW81642-A180',
+    name: '4 × 4 MP Multi-Sensor Panoramic Bullet Wizmind Network Camera',
+    image: '/src/assets/WizMind/img2.png',
+
+
+    description: `
+    • Four 4-MP 1/1.8" CMOS image sensors, low luminance, and high definition image.
+    • Outputs max. 16 MP (5520 × 2700)@25/30 fps.
+    • H.265 codec, high compression rate, ultra-low bit rate.
+    • Three streams for HD video display.
+    • Built-in IR LED, and the max. illumination distance is 30 m.
+    • 3D NR, HLC, BLC, digital watermarking, applicable to various monitoring scenes.
+    • ROI, SMART H.264+/H.265+, flexible coding, applicable to various bandwidth and storage environments.
+    • Alarm: 2 in, 2 out; audio: 1 in, 1 out; RS-485; BNC; supports max. 256 G Micro SD card.
+    • 12 VDC/24 VAC/PoE power supply, easy for installation.
+    • IP67 and IK10 protection.
+`,
+    category: 'Panoramic Series',
+    subCategory: 'Multi-Sensor 180° Series',
+    brochure: '/brochures/brousher.pdf',
+    additionalImage: '/src/assets/descimg/img5.png'
+  },
+
+
+  // Panoramic Series -> Multi-Directional Series -> 2 Products
+
+  
+
+
+
+
+
+
+
+  // Add more products as needed
+];
+
+export function getProductData(slug) {
+  return products.find((product) => product.id === slug);
+}
+
+export function getAllProducts() {
+  return products.map((product) => product.id);
+}
+
+export { products };
